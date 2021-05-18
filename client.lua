c1bda8aedda64d8d81535ef6c1a1feefbb0901f9
Dalrae1 <52721173+Dalrae1@users.noreply.github.com>
2021-05-18 14:25:01 -0700

    Added support for expanding the minimap

--- client.lua
+++ client.lua
@@ -1,14 +1,21 @@
 function getMapPosition()
 	local minimap = {}
-	SetScriptGfxAlign(string.byte('L'), string.byte('B'))
-	local minimapRawX, minimapRawY = GetScriptGfxPosition(-0.0045, 0.002 + (-0.188888))
-	ResetScriptGfxAlign()
 	local resX, resY = GetActiveScreenResolution()
 	local aspectRatio = GetAspectRatio()
 	local scaleX = 1/resX
 	local scaleY = 1/resY
-	minimap.width = scaleX*(resX/(4*aspectRatio))
-	minimap.height = scaleY*(resY/(5.674))
+	local minimapRawX, minimapRawY
+	SetScriptGfxAlign(string.byte('L'), string.byte('B'))
+	if IsBigmapActive() then
+		minimapRawX, minimapRawY = GetScriptGfxPosition(-0.003975, 0.022 + (-0.460416666))
+		minimap.width = scaleX*(resX/(2.52*aspectRatio))
+		minimap.height = scaleY*(resY/(2.3374))
+	else
+		minimapRawX, minimapRawY = GetScriptGfxPosition(-0.0045, 0.002 + (-0.188888))
+		minimap.width = scaleX*(resX/(4*aspectRatio))
+		minimap.height = scaleY*(resY/(5.674))
+	end
+	ResetScriptGfxAlign()
 	minimap.leftX = minimapRawX
 	minimap.rightX = minimapRawX+minimap.width
 	minimap.topY = minimapRawY
